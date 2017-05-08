5d07b9972259af51cb0f7de72e342cf6023f65bc
nbekris <nikobekris@gmail.com>
2017-05-07 19:30:47 -0700

    Add files via upload

--- HomeWork4/app/src/main/java/niko/homework4/RecyclerView.java
+++ HomeWork4/app/src/main/java/niko/homework4/RecyclerView.java
@@ -2,15 +2,20 @@ package niko.homework4;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class RecyclerView extends AppCompatActivity {
-    Button button;
+    private static final String TAG = RecyclerView.class.getSimpleName();
 
     Context context;
     android.support.v7.widget.RecyclerView recyclerView;
@@ -35,8 +40,14 @@ public class RecyclerView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar3);
+        setSupportActionBar(mToolbar);
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         context = getApplicationContext();
 
@@ -52,14 +63,40 @@ public class RecyclerView extends AppCompatActivity {
 
         recyclerView.setAdapter(recyclerViewAdapter);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(myhandler1);
 
     }
-    View.OnClickListener myhandler1 = new View.OnClickListener() {
-        public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuInflater = getMenuInflater();
+        mMenuInflater.inflate(R.menu.my_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.home) {
+            // Respond to the action bar's Up/Home button
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        if (item.getItemId() == R.id.action_setting) {
+            Log.d(TAG, "User clicked action_setting");
+            Toast.makeText(RecyclerView.this,
+                    "You have clicked on setting action menu",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+        if (item.getItemId() == R.id.action_about_us) {
+            Log.d(TAG, "User clicked action_about_us");
+            Toast.makeText(RecyclerView.this,
+                    "You have clicked on about us action menu",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+        if (item.getItemId() == R.id.action_home) {
             Intent intent = new Intent(RecyclerView.this, MainActivity.class);
             startActivity(intent);
         }
-    };
+        return super.onOptionsItemSelected(item);
+    }
 }
